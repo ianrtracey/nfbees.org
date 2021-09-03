cda5155b80abfd48687697153b2c3bcb4e4eb6e7
Ian Tracey <ianrtracey@gmail.com>
2021-09-03 19:07:56 -0400

    Adds bee favicon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>NFBees</title>
         <meta name="description" content="NFBees" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐝</text></svg>" />
       </Head>
 
       <main>
